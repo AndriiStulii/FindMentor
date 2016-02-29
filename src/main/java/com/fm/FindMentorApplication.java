6eecc2c95d65032b129db4b4ea256fdc464aab2e
AndriiStulii <andrewthor16@gmail.com>
2016-02-29 21:34:21 +0200

    init

--- src/main/java/com/fm/FindMentorApplication.java
+++ src/main/java/com/fm/FindMentorApplication.java
@@ -2,8 +2,9 @@ package com.fm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class FindMentorApplication {
 
 	public static void main(String[] args) {
