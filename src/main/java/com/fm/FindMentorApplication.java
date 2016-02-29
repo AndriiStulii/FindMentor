77836c4e2f1eff4d92718fce53612fc9721b503e
AndriiStulii <andrewthor16@gmail.com>
2016-02-29 21:22:19 +0200

    Initial commit

---
+++ src/main/java/com/fm/FindMentorApplication.java
@@ -0,0 +1,12 @@
+package com.fm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMentorApplication.class, args);
+	}
+}
